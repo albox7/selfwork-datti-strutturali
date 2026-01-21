a02f20e7f0443b8b77bb52448785bea7f5fc18d5
Albox <albox7@gmail.com>
2026-01-21 20:19:17 +0100

    Estrazione dati da array

---
+++ main.js
@@ -0,0 +1,36 @@
+
+
+let array_1 = [
+  ['Un', 'un', 'per', 'incatenarli.'],
+  ['Anello', 'trovarli,'],
+  ['ghermirli', 'e'],
+  ['gondor', 'mark'],
+];
+
+let array_2 = [
+  [['trovarli,']],
+  ['tu,', 'sciocchi'],
+  ['tu,', 'sciocchi', ['padron', 'Sauron']],
+  ['nel', ['fuggite', 'gandalf']],
+  [['domarli,', 'passare'], 'buio']
+];
+
+
+
+console.log(array_1[0][0], // Un
+			array_1[1][0], // Anello
+			array_1[0][1], // per
+			array_2[4][0][0], // domarli,
+			array_1[0][1], // un
+			array_1[1][0], // Anello
+			array_1[0][1], // per
+			array_1[1][1], // trovarli,
+			array_1[0][1], // un
+			array_1[1][0], // Anello
+			array_1[0][2], // per
+			array_1[2][0], // ghermirli
+			array_1[2][1], // e
+			array_2[3][0], // nel
+			array_2[4][1], // buio
+			array_1[0][3], // incatenarli
+			);
